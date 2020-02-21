bfb37c0834805d50067cdabcb4d9debcfbb40f20
shruti2286 <47882761+shruti2286@users.noreply.github.com>
2020-02-20 20:14:17 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -26,26 +26,6 @@ map.addControl(new mapboxgl.NavigationControl());
 cnndata.forEach(function(foodEntry) {
   // for each object in the studentData, add a marker to the map with a popup
   new mapboxgl.Marker()
-  .Marker(width 30px
-  height 30px
-  border-radius 50% 50% 50% 0
-  background #89849b
-  position absolute
-  transform rotate(-45deg)
-  left 50%
-  top 50%
-  margin -20px 0 0 -20px
-  animation-name bounce
-  animation-fill-mode both
-  animation-duration 1s
-  &:after
-    content ''
-    width 14px
-    height 14px
-    margin 8px 0 0 8px
-    background #2F2F2F
-    position absolute
-    border-radius 50%)
     .setLngLat([foodEntry.longitude, foodEntry.latitude])
     .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
      .setHTML(`${foodEntry.name} goes to ${foodEntry.food_item} from ${foodEntry.country}🏆`))
